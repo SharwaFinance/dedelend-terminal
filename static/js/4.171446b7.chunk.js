a9196eb2d53ffa082a95efdd3e07269eb33e46e1
whackamole7 <whackamole7@proton.me>
2023-04-12 15:21:35 +0300

    Updates

--- static/js/4.171446b7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpdedelend-terminal"]=this["webpackJsonpdedelend-terminal"]||[]).push([[4],{922:function(e,n){}}]);
-//# sourceMappingURL=4.171446b7.chunk.js.map
---
+++ static/js/4.e753a10e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpdedelend-terminal"]=this["webpackJsonpdedelend-terminal"]||[]).push([[4],{924:function(e,n){}}]);
+//# sourceMappingURL=4.e753a10e.chunk.js.map
